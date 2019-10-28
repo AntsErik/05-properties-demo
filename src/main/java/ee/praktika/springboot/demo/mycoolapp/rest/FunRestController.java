cc97ac3bee9bf614c74dd5fdf3e3c94f0b598814
AntsErik <ants.erik.noormagi@aweroc.com>
2019-10-28 09:37:20 +0200

    Defined application.properties file values and added field to FunRestController + mapping to display on the server.

--- src/main/java/ee/praktika/springboot/demo/mycoolapp/rest/FunRestController.java
+++ src/main/java/ee/praktika/springboot/demo/mycoolapp/rest/FunRestController.java
@@ -2,31 +2,42 @@ package ee.praktika.springboot.demo.mycoolapp.rest;
 
 import java.time.LocalDateTime;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class FunRestController {
-	
-	//expose a slash endpoint that will return "Hello World!"
-	@GetMapping("/")
-	public String sayHello() {
-		return "Hello World! Time on server is " + LocalDateTime.now();
-	}
-	
-	//adding a new mapping to test DevTools.
-	@GetMapping("/workout")
-	public String getDailyWorkout() {
-		return "Run a hard 5 K";
-	}
-	
-	//adding a second endpoint for testing - fortune
-	@GetMapping("/fortune")
-	public String getFortune() {
-		return "Today is allrighty!";
-	}
-	
-	
+
+    //inject properties for coach.name and team.name
+    @Value( "${coach.name}" )
+    private String coachName;
+
+    @Value( "${team.name}" )
+    private String teamName;
+
+    //expose new endpoint for "teaminfo"
+    @GetMapping( "/teaminfo" )
+    public String getTeamInfo(){
+        return "Coach: " + coachName + ", Team name: " + teamName;
+    }
+
+    //expose a slash endpoint that will return "Hello World!"
+    @GetMapping( "/" )
+    public String sayHello(){
+        return "Hello World! Time on server is " + LocalDateTime.now();
+    }
+
+    //adding a new mapping to test DevTools.
+    @GetMapping( "/workout" )
+    public String getDailyWorkout(){
+        return "Run a hard 5 K";
+    }
+
+    //adding a second endpoint for testing - fortune
+    @GetMapping( "/fortune" )
+    public String getFortune(){
+        return "Today is allrighty!";
+    }
 
 }
-	
